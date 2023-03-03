f482399091446ac152a5a4e0290ed43983a050e3
nada <nadasamir9334@gmail.com>
2023-03-02 19:51:22 -0500

    search card done

--- src/Components/Searchcard.js
+++ src/Components/Searchcard.js
@@ -8,59 +8,107 @@ import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Accordion from 'react-bootstrap/Accordion';
+import { FaBusAlt } from 'react-icons/fa'
+import { BsClockFill } from 'react-icons/bs'
 
 function Searchcard() {
     return (
-        <Card >
-            <Row>
-                <Col md='9'>
-                    <Row>
-                        <Col md='2'></Col>
-                        <Col>
-                            <div>
-                                3:00PM<span>Union Station </span>
-                            </div>
-                            <div>
-                                7:20 PM
-                                <span>
-                                    Port Authority Bus Station </span>
-                            </div>
-                        </Col>
+        <>
+            <Card className=' p-3 mb-5'>
+                <Row>
+                    <Col md='9'>
+                        <Row>
 
-                    </Row>
-                    <div className='d-flex'>
+                            <Col>
+                                <div>
+                                    3:00PM<span>Union Station </span>
+                                </div>
+                                <div>
+                                    7:20PM
+                                    <span>
+                                        Port Authority Bus Station </span>
+                                </div>
+                            </Col>
 
-                        <Accordion defaultActiveKey="0" className=''>
+                        </Row>
+                        <div className='d-flex mt-5 '>
+
+                            {/* <Accordion defaultActiveKey="0" className=''>
                             <Accordion.Item eventKey="0">
                                 <Accordion.Header>Accordion Item #1</Accordion.Header>
                                 <Accordion.Body>
 
                                 </Accordion.Body>
                             </Accordion.Item>
-                        </Accordion>
+                        </Accordion> */}
 
-                        <Badge pill bg="warning" text="dark" className='h-100'>
-                            Warning
-                        </Badge>
-                        <Badge pill bg="warning" text="dark" className='h-100'>
-                            Warning
-                        </Badge>
+                            <div className='d-flex  '>
+                                <FaBusAlt style={{ color: "green" }} />
+                                <p className='mx-2'>Bus</p>
+                            </div>
+                            <div className='d-flex mx-3 '>
+                                <BsClockFill />
+                                <p className='mx-2'>55m</p>
+                            </div>
+                        </div>
 
+                    </Col>
+                    <Col md='3' className='text-end' >
+                        <div className='prise mb-5'>$<span>16</span></div>
+                        <button type="button" className="btn btn-outline-success">Select</button>
 
+                    </Col>
+                </Row>
+            </Card>
+            <Card className=' p-3 mb-5'>
+                <Row>
+                    <Col md='9'>
+                        <Row>
 
+                            <Col>
+                                <div>
+                                    3:00PM<span>Union Station </span>
+                                </div>
+                                <div>
+                                    7:20PM
+                                    <span>
+                                        Port Authority Bus Station </span>
+                                </div>
+                            </Col>
 
+                        </Row>
+                        <div className='d-flex mt-5 '>
 
+                            {/* <Accordion defaultActiveKey="0" className=''>
+                            <Accordion.Item eventKey="0">
+                                <Accordion.Header>Accordion Item #1</Accordion.Header>
+                                <Accordion.Body>
 
-                    </div>
+                                </Accordion.Body>
+                            </Accordion.Item>
+                        </Accordion> */}
 
-                </Col>
-                <Col md='3' >
-                    <div className='prise'>$<span>16</span></div>
-                    <Button variant="secondary">Secondary</Button>
+                            <div className='d-flex  '>
+                                <FaBusAlt style={{ color: "green" }} />
+                                <p className='mx-2'>Bus</p>
+                            </div>
+                            <div className='d-flex mx-3 '>
+                                <BsClockFill />
+                                <p className='mx-2'>55m</p>
+                            </div>
+                        </div>
 
-                </Col>
-            </Row>
-        </Card>
+                    </Col>
+                    <Col md='3' className='text-end' >
+                        <div className='prise mb-5'>$<span>16</span></div>
+                        <button type="button" className="btn btn-outline-success">Select</button>
+
+                    </Col>
+                </Row>
+            </Card>
+
+        </>
+
 
     )
 }
